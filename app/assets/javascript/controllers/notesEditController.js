91a0934d80a3dc16d76ef9d9a163ea17097ffdef
Alejandro Quadri <alejandroquadri@quadri.com.ar>
2015-11-23 23:21:12 -0300

    Agregado restriccion para submit y vuelta al show. Min 1:07:00

--- app/assets/javascript/controllers/notesEditController.js
+++ app/assets/javascript/controllers/notesEditController.js
@@ -1,7 +1,17 @@
-angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams){
+angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams,$location){
  console.log('anda el edit');
  $scope.note = Note.get({id: $routeParams.id});
+ $scope.isSubmitting = false;
+
  $scope.saveNote = function(note){
-   note.$update(); //este metodo esta definido especialmente en el $resource (en note.js), el signo $ se lo pone solo
+   $scope.isSubmitting = true;
+   note.$update() //este metodo esta definido especialmente en el $resource (en note.js), el signo $ se lo pone solo
+    .finally(function(){
+      $scope.isSubmitting = false; // esto es para que una vez que termina el update vuelva a habilitarse.
+      $location.path("/notes/"+note.id);
+      //este servicio, location, no lo explicaron en el curso. Atencion lo inyecta arriba
+      //poniendo eso te manda una vez terminado a la pagina show.
+      //se puede usar note.id, porque se paso en la funcion la instancia note
+    })
  }
 });
