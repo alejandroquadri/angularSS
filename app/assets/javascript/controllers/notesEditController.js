c8c1b049a26b3ccb3b71c53aaa9c65f94e3f5ae2
Alejandro Quadri <alejandroquadri@quadri.com.ar>
2015-11-23 22:52:31 -0300

    Terminado el edit, 1:02:00

--- app/assets/javascript/controllers/notesEditController.js
+++ app/assets/javascript/controllers/notesEditController.js
@@ -1,4 +1,7 @@
 angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams){
  console.log('anda el edit');
  $scope.note = Note.get({id: $routeParams.id});
+ $scope.saveNote = function(note){
+   note.$update(); //este metodo esta definido especialmente en el $resource (en note.js), el signo $ se lo pone solo
+ }
 });
--- app/assets/javascript/services/note.js
+++ app/assets/javascript/services/note.js
@@ -1,4 +1,10 @@
 angular.module('NoteWrangler')
 .factory('Note', function($resource){
-    return $resource('/notes/:id');
+    return $resource('/notes/:id', {id: "@id"}, {
+      //en la inea de arriba primero se pone la direccion. Hay que aclarar que parametro es id.
+      // el segundo objeto lo que hace es casualmente eso. poniendo @id estamos diciendo que id corresponde al id de esta instancia
+      update: {
+        method: "PUT"
+      }
+    });
 });
