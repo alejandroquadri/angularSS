74658e6061342b5be1cf083e22f1fd03c73e7eb9
Alejandro Quadri <alejandroquadri@gmail.com>
2015-12-03 19:27:30 -0300

    Agregado dropdown Usuarios. min 1.46.00

--- app/assets/javascript/controllers/notesEditController.js
+++ app/assets/javascript/controllers/notesEditController.js
@@ -1,7 +1,8 @@
-angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, Category, $routeParams,$location){
+angular.module('NoteWrangler').controller('NotesEditController', function(Note, User, $scope, Category, $routeParams,$location){
  $scope.note = Note.get({id: $routeParams.id});
  $scope.isSubmitting = false;
  $scope.categories = Category.query();
+ $scope.users = User.query();
 
  $scope.saveNote = function(note){
    $scope.isSubmitting = true;
