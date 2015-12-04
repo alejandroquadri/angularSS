d78f75d4910070be8d4e51e02382b2309fd5ea0a
Alejandro Quadri <alejandroquadri@gmail.com>
2015-12-04 17:52:13 -0300

    hasta 1.56.30

--- app/assets/javascript/directives/nwCategorySelector.js
+++ app/assets/javascript/directives/nwCategorySelector.js
@@ -1,10 +1,18 @@
-angular.module('NoteWrangler').directive('nwCategorySelector',function(){
+angular.module('NoteWrangler').directive("nwCategorySelector", function(Category){
   return {
     replace: "true",
     restrict: "E",
-    templateUrl: "/assets/templates/directives/nwCategorySelector.html"
-    link: function (){
-      
+    templateUrl: "/assets/templates/directives/nwCategorySelector.html",
+    link: function (scope, element, attrs){
+      var activeCategory = {};
+      scope.categories = Category.query();
+
+      scope.isActive = function (category){
+        return activeCategory.id === category.id
+      };
+      scope.toggleCategory = function (category){
+
+      };
     }
   };
 });
