6cf4d8b4a549e88cb89851e3dfdf70e3e928dd33
Alejandro Quadri <alejandroquadri@quadri.com.ar>
2015-12-03 23:41:28 -0300

    hasta 1.51.00

---
+++ app/assets/javascript/directives/nwCategorySelector.js
@@ -0,0 +1,10 @@
+angular.module('NoteWrangler').directive('nwCategorySelector',function(){
+  return {
+    replace: "true",
+    restrict: "E",
+    templateUrl: "/assets/templates/directives/nwCategorySelector.html"
+    link: function (){
+      
+    }
+  };
+});
